8d195392815352910e8efc4785b022d8b5fa491f
Foysol Ahmed <foysol.sahi@gmail.com>
2021-04-14 12:10:42 +0600

    initialization and setup/cleaning

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import { useEffect } from 'react';
+
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
-import { useEffect } from 'react';
 import './App.css';
 
 const App = () => {
