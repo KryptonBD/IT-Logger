8a06cb54f4ea1289bc0a3163c522bb3cb5c3eebd
Foysol Ahmed <foysol.sahi@gmail.com>
2021-04-15 03:32:17 +0600

    UI Done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import { Fragment, useEffect } from 'react';
 
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
+import AddBtn from './components/layout/AddBtn';
+
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
@@ -15,6 +21,11 @@ const App = () => {
     <Fragment>
       <SearchBar/>
       <div className="container">
+        <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
         <Logs/>
       </div>
     </Fragment>
